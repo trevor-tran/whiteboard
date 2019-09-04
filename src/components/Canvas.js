f2e7c878d4f3f3887c99a9e1523022ad10527972
Phuong Tran <ptran816@uw.edu>
2019-09-04 13:24:48 -0700

    canvas is drawable but to slow

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -8,8 +8,10 @@ import { Context } from '../store/store'
 
 function Canvas() {
   const { state } = useContext(Context)
+  const [is_drawing, setIsDrawing] = useState(false)
   const [dots, setDots] = useState([])
   const canvasRef = useRef(null)
+
 
   useEffect(() => {
     const canvas = canvasRef.current
@@ -18,7 +20,7 @@ function Canvas() {
     canvas.height = canvas.offsetHeight
     const ctx = canvas.getContext('2d')
     redraw(ctx)
-  }, dots, state)
+  }, [dots, state])
 
   const redraw = (ctx) => {
     ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height)
@@ -26,19 +28,45 @@ function Canvas() {
     ctx.lineWidth = state.width
     if (dots.length !== 0) {
       ctx.beginPath()
-      ctx.moveTo(dots[0][0], dots[0][1])
+      ctx.moveTo(dots[0].x, dots[0].y)
       dots.forEach(dot => {
-        ctx.lineTo(dot[0], dot[1])
+        ctx.lineTo(dot.x, dot.y)
         ctx.stroke()
       })
     }
   }
 
+  const startDrawing = (e) => {
+    setIsDrawing(true)
+    const canvas = canvasRef.current
+    let pos = getMousePos(canvas, e)
+    setDots(prev => [...prev, pos])
+  }
+  const drawing = (e) => {
+    if (is_drawing) {
+      const canvas = canvasRef.current
+      let pos = getMousePos(canvas, e)
+      console.log(pos)
+      setDots(prev => [...prev, pos])
+    }
+  }
+
+  // https://stackoverflow.com/questions/17130395/real-mouse-position-in-canvas
+  function getMousePos(canvas, evt) {
+    var rect = canvas.getBoundingClientRect();
+    return {
+      x: evt.clientX - rect.left,
+      y: evt.clientY - rect.top
+    }
+  }
   return (
     <Paper className="canvas">
       <canvas
         ref={canvasRef}
         style={{ width: '100%', height: '100%' }}
+        onMouseDown={startDrawing}
+        onMouseMove={drawing}
+        onMouseUp={() => {setIsDrawing(false)}}
       />
     </Paper>
   )
