3f03edb7bbeac8808ade4cee30c12db912fe88f4
Phuong Tran <ptran816@uw.edu>
2019-09-05 00:58:52 -0700

    removed most of the things in template, only need app.js

--- server/app.js
+++ server/app.js
@@ -1,26 +1,25 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const app = express();
+const socket = require('socket.io');
+const http = require('http');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// listening port
+const PORT = process.env.PORT || 8080;
+
+const server = http.createServer(app);
+
+server.listen(PORT, ()=>{
+  console.log(`Listening on port ${PORT}`);
+});
+
+app.get('/test', (req, res) => {
+  res.json({
+    message: err.message,
+    error: err
+  })
+})
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
