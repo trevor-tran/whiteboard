024b9a1fda2d6b2831f13f4f5df3b16a04843d11
Phuong Tran <ptran816@uw.edu>
2019-09-11 17:06:49 -0700

    responive to window size

--- client/src/components/Display.js
+++ client/src/components/Display.js
@@ -1,4 +1,9 @@
-import React, { useReducer } from 'react'
+import React, { useReducer, useState } from 'react'
+import { useMediaQuery } from 'react-responsive'
+
+import { Fab } from '@material-ui/core'
+import DownIcon from '@material-ui/icons/KeyboardArrowDown';
+import UpIcon from '@material-ui/icons/KeyboardArrowUp';
 
 import { Context, initialState, reducer } from '../store/store'
 
@@ -7,13 +12,25 @@ import Tool from './Tool'
 import Canvas from './Canvas'
 
 function Display() {
-  
   const [state, dispatch] = useReducer(reducer, initialState)
+
+  const [showTool, setShowTool] = useState(false)
+  const IsDesktop = useMediaQuery({ minWidth: 1024 })
+  const IsMobile = useMediaQuery({ maxWidth: 1024 })
+
   return (
     <Context.Provider value={{ state, dispatch }}>
       <React.Fragment>
-        <Tool />
-        <Canvas style={{overflow:'hidden'}}/>
+        {(IsDesktop || showTool) && <Tool />}
+        {!IsDesktop && (
+          <Fab
+            size="small"
+            style={{ position: 'absolute', bottom: '1%', right: '1%', backgroundColor: '#6aaf6a', color: 'white' }}
+            onClick={() => setShowTool(!showTool)}>
+            {showTool ? <DownIcon /> : <UpIcon />}
+          </Fab>
+        )}
+        <Canvas />
       </React.Fragment>
     </Context.Provider>
   )
