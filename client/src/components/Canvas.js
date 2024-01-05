6290364d9d65229cb933ccc0a7b6ca223c8382ee
Trevor Tran <fuongdtran@gmail.com>
2024-01-05 01:36:18 -0800

    put all shapes in one array for easy undo

--- client/src/components/Canvas.js
+++ client/src/components/Canvas.js
@@ -5,132 +5,169 @@ import paper from 'paper';
 import io from 'socket.io-client'
 import { server, CanvasConsts } from '../consts'
 
-const socket = io(server.URL)
+import {
+  Stage,
+  Layer,
+  Rect,
+  Circle,
+  Line
+} from 'react-konva';
+
+// const socket = io(server.URL)
+
+import { type } from '../utils/const';
 
 function Canvas() {
-  const canvasRef = useRef(null)
-  // this is global state. Go to ../store/store.js to see what state is available
-  const { state, dispatch } = useContext(Context)
-  // is user drawing?
-  const [is_drawing, setIsDrawing] = useState(false)
-  // PaperJS Path object
-  const paperPath = useRef(null);
-  // html5 canvas id
-  const canvasId = useRef("drawing-canvas");
+  const layerRef = useRef();
+  const isDrawing = useRef(false);
 
+  const [tool, setTool] = useState(type.LINE);
+  const [shapes, setShapes] = useState([]);
+  const [currentColor, setCurrentColor] = useState("#5300eb");
 
-  // call once on initial render
-  useEffect(() => {
-    paper.setup(canvasId.current);
-  },[]);
-
-  // clear canvas
-  useEffect(() => {
-    if (state.clear) {
-      dispatch({ type: types.SET_CLEAR, payload: false })
-      socket.emit(CanvasConsts.BROADCAST, { state: state, segments: [] });
-      // https://stackoverflow.com/questions/19054798/canvas-clear-in-paper-js
-      paper.project.activeLayer.removeChildren();
-      paper.view.draw();
+  const handleMouseDown = (e) => {
+    isDrawing.current = true;
+    const pos = e.target.getStage().getPointerPosition();
+    let shape;
+    if (tool === type.CIRCLE) {
+      shape = { type: tool, color: currentColor, center: pos, radius: 0 };
+    } else if (tool === type.RECT) {
+      shape = { type: tool, color: currentColor, topLeft: pos, width: 0, height: 0 };
+    } else if (tool === type.LINE) {
+      shape = { type: type.LINE, color: currentColor, points: [pos.x, pos.y] };
+    } else {
+      shape = { type: type.FREE_LINE, color: currentColor, points: [pos.x, pos.y] };
     }
-  }, [state.clear, dispatch])
 
-  //listening on server socket
-  useEffect(() => {
-    const canvas = canvasRef.current
-    const ctx = canvas.getContext('2d')
+    setShapes([...shapes, shape]);
+  };
 
-    socket.on(state.room, data => {
-      if (data.state.clear) {
-        // clear the entire canvas
-        paper.project.activeLayer.removeChildren();
-        paper.view.draw();
-        return;
-      }
-      // set color and width which are sent from the host (who shares the canvas)
-      const {color, width} = data.state;
-      ctx.strokeStyle = color;
-      ctx.lineWidth = width;
-      paperPath.current = new paper.Path({strokeColor: color, strokeWidth: width});
-      const segments = data.segments.map(element => {
-        let seg = JSON.parse(element);
-        seg.point.x = seg.point.x / data.canvasWidth * canvas.width;
-        seg.point.y = seg.point.y / data.canvasHeight * canvas.height;
-        return seg;
-      });
-      paperPath.current.segments = segments;
-    })
-  }, [state.room])
-
-  // triggered when the mouse pressed down
-  // get current mouse's location, and signal drawing has begun
-  const onMouseDown = (e) => {
-    setIsDrawing(true);
-    const canvas = canvasRef.current;
-    let mousePos = getMousePos(canvas, e);
-
-    paperPath.current = new paper.Path({strokeColor: state.color, strokeWidth: state.width});
-    paperPath.current.segments = [mousePos];
-  }
-
-  // draw lines to current mouse location
-  const onMouseDrag = (e) => {
-    if (!is_drawing) {
+  const handleMouseMove = (e) => {
+    // no drawing - skipping
+    if (!isDrawing.current) {
       return;
     }
-    const canvas = canvasRef.current;
-    let mousePos = getMousePos(canvas, e);
-    paperPath.current.add(mousePos);
-  }
+    const currentPointer = e.target.getStage().getPointerPosition();
 
-  // signal drawing has stoped
-  const onMouseUp = () => {
-    const canvas = canvasRef.current;
-    setIsDrawing(false);
-    paperPath.current.simplify(10);
-
-    if (state.room) {
-          const segments = paperPath.current.segments.map( seg => {
-            // insert double quotes to each object key
-            let s  = seg.toString();
-            return s.replace(/([a-zA-Z])+/g,'"$&"');
-          });
-      socket.emit(CanvasConsts.BROADCAST, { state: state, segments: segments, canvasWidth: canvas.width, canvasHeight: canvas.height})
-    }
-  }
-
-  // https://stackoverflow.com/questions/17130395/real-mouse-position-in-canvas
-  function getMousePos(canvas, evt) {
-    var rect = canvas.getBoundingClientRect();
-    if (evt.changedTouches) {
-      return [
-        (evt.changedTouches[0].clientX - rect.left),
-        (evt.changedTouches[0].clientY - rect.top),
-      ]
+    let lastShape = shapes[shapes.length - 1];
+    let updatedLastShape;
+    if (tool === type.CIRCLE) {
+      updatedLastShape = drawCircle(lastShape, currentPointer);
+    } else if (tool === type.RECT) {
+      updatedLastShape = drawRect(lastShape, currentPointer);
+    } else if (tool === type.LINE) {
+      updatedLastShape = drawLine(lastShape, currentPointer);
+    } else {
+      updatedLastShape = drawFree(lastShape, currentPointer);
     }
 
-    return [
-      (evt.clientX - rect.left),
-      (evt.clientY - rect.top),
-    ]
-  }
+    // replace last
+    shapes.splice(shapes.length - 1, 1, updatedLastShape);
+    setShapes(shapes.concat());
+  };
+
+  const handleMouseUp = () => {
+    isDrawing.current = false;
+  };
 
   return (
-    <div className='canvas'   style={{width:"100vw", height: "100vh",  cursor: "pointer"}}>
-      <canvas
-        id={canvasId.current}
-        ref={canvasRef}
-        style={{ width: '100%', height: '100%' }}
-        resize="true"
-        onMouseDown={onMouseDown}
-        onMouseMove={onMouseDrag}
-        onMouseUp={onMouseUp}
-        onTouchStart={onMouseDown}
-        onTouchMove={onMouseDrag}
-        onTouchEnd={onMouseUp}
-      />
-    </div>
+    <Stage
+      width={window.innerWidth}
+      height={window.innerHeight}
+      onMouseDown={handleMouseDown}
+      onMousemove={handleMouseMove}
+      onMouseup={handleMouseUp}
+    >
+      <Layer ref={layerRef}>
+        {shapes.map((shape, i) => {
+          console.log(shape)
+          if (shape.type === type.CIRCLE) {
+            return (
+              <Circle
+                key={i}
+                x={shape.center.x}
+                y={shape.center.y}
+                radius={shape.radius}
+                stroke={shape.color}
+              />
+            )
+          } else if (shape.type === type.RECT) {
+            return (
+              <Rect
+                key={i}
+                x={shape.topLeft.x}
+                y={shape.topLeft.y}
+                width={shape.width}
+                height={shape.height}
+                stroke={shape.color}
+              />
+            )
+          } else {
+            return (
+              <Line
+                key={i}
+                points={shape.points}
+                stroke={"#5300eb"}
+                strokeWidth={1}
+              // tension={0.5}
+              />
+            )
+          }
+        })}
+      </Layer>
+    </Stage>
   )
+};
+
+export default React.memo(Canvas);
+
+const calcDistance = (pointA, pointB) => {
+  const powerOfX = Math.pow(Number(pointB.x) - Number(pointA.x), 2);
+  const powerOfY = Math.pow(Number(pointB.y) - Number(pointA.y), 2);
+
+  return Math.sqrt(powerOfX + powerOfY);
+};
+
+function drawFree(lastFreeLine, currentPointerPos) {
+  let updatedLastFreeLine = { ...lastFreeLine };
+
+  updatedLastFreeLine.points.push(currentPointerPos.x);
+  updatedLastFreeLine.points.push(currentPointerPos.y);
+
+  return updatedLastFreeLine;
+};
+
+function drawLine(lastLine, currentPointerPos) {
+  let updatedLastLine = { ...lastLine };
+
+  if (updatedLastLine.points.length == 4) {
+    updatedLastLine.points.splice(lastLine.points.length - 2, 2);
+  }
+  updatedLastLine.points.push(currentPointerPos.x);
+  updatedLastLine.points.push(currentPointerPos.y);
+
+  return updatedLastLine;
 }
 
-export default React.memo(Canvas)
+function drawRect(lastRect, currentPointerPos) {
+  let updatedLastRect = { ...lastRect };
+  const { topLeft } = updatedLastRect;
+
+  // compute new width and height
+  const newWidth = Math.abs(currentPointerPos.x - topLeft.x);
+  const newHeight = Math.abs(currentPointerPos.y - topLeft.y);
+
+  updatedLastRect.width = newWidth;
+  updatedLastRect.height = newHeight;
+
+  return updatedLastRect;
+};
+
+function drawCircle(lastCircle, currentPointerPos) {
+  let updatedLastCircle = { ...lastCircle };
+
+  const newRadius = calcDistance(updatedLastCircle.center, currentPointerPos);
+  updatedLastCircle.radius = newRadius;
+
+  return updatedLastCircle;
+};
